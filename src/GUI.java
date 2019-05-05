21d5c5bbc77d7b97301a7a4e7e1b3a6d365944fb
Sam <samuelrcain@gmail.com>
2019-05-05 17:54:20 -0400

    Everything except comment persistence is done

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,10 @@
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -22,8 +22,9 @@ public class GUI extends JFrame {
 	private JList<Application> list; // List containing all apps
 	private JList<String> commentList, requestList;
 	private DefaultListModel<Application> model;
-	private DefaultListModel<String> comments;
+	private DefaultListModel<String> comments, requestsModel;
 	private Database db;
+	private JRadioButton guestRadio, adminRadio, modRadio;
 	private User user;
 
 	/**
@@ -39,38 +40,44 @@ public class GUI extends JFrame {
 		add(panel);
 	}
 
+	public void addComponents(Component... components) {
+		for (Component component : components) {
+			panel.add(component);
+		}
+		repaint();
+		revalidate();
+	}
+
 	public void logIn() {
 		panel.removeAll();
 
+		ActionListener lIListener = new logInListener();
+		ActionListener createListener = new createAccountFormListener();
+
+		// Button to create account
 		JButton createButton = new JButton();
 		createButton.setText("Create Account");
 		createButton.setSize(200, 25);
 		createButton.setLocation(100, 300);
+		createButton.addActionListener(createListener);
 
+		// Textfields used for username and password
+		tBox1.setLocation(250, 100);
+		tBox1.setSize(200, 25);
 		tBox1.setText("Username");
 
 		tBox2.setLocation(250, 175);
 		tBox2.setSize(200, 25);
 		tBox2.setText("Password");
 
+		// Button to log in
 		JButton logInButton = new JButton();
 		logInButton.setText("Log In");
 		logInButton.setSize(100, 25);
 		logInButton.setLocation(100, 137);
-
-		ActionListener lIListener = new logInListener();
-		ActionListener createListener = new createAccountFormListener();
-
 		logInButton.addActionListener(lIListener);
-		createButton.addActionListener(createListener);
 
-		panel.add(tBox1);
-		panel.add(tBox2);
-		panel.add(logInButton);
-		panel.add(createButton);
-
-		repaint();
-		revalidate();
+		addComponents(tBox1, tBox2, logInButton, createButton);
 	}
 
 	public void createAccountForm() {
@@ -83,17 +90,27 @@ public class GUI extends JFrame {
 		JButton createButton = new JButton();
 		createButton.setText("Create");
 		createButton.setSize(100, 25);
-		createButton.setLocation(100, 300);
+		createButton.setLocation(270, 400);
+
+		ButtonGroup radioButtons = new ButtonGroup();
+
+		guestRadio = new JRadioButton("Guest ");
+		guestRadio.setBounds(250, 300, 75, 25);
+		guestRadio.setSelected(true);
+		radioButtons.add(guestRadio);
+
+		adminRadio = new JRadioButton("Admin ");
+		adminRadio.setBounds(325, 300, 75, 25);
+		radioButtons.add(adminRadio);
+
+		modRadio = new JRadioButton("Mod ");
+		modRadio.setBounds(400, 300, 75, 25);
+		radioButtons.add(modRadio);
 
 		ActionListener create = new createAccountListener();
 		createButton.addActionListener(create);
 
-		panel.add(tBox1);
-		panel.add(tBox2);
-		panel.add(createButton);
-
-		repaint();
-		revalidate();
+		addComponents(tBox1, tBox2, guestRadio, adminRadio, modRadio, createButton);
 	}
 
 	/**
@@ -112,7 +129,7 @@ public class GUI extends JFrame {
 		tBox1 = new JTextField();
 		tBox1.setLocation(250, 100);
 		tBox1.setSize(200, 25);
-		
+
 		tBox2 = new JTextField();
 
 		ActionListener listener = new loadListener();
@@ -147,19 +164,27 @@ public class GUI extends JFrame {
 		ActionListener sCListener = new sortCompanyListener();
 		ActionListener vCListener = new viewCommentsListener();
 		ActionListener sRListener = new submitRequestViewListener();
+		ActionListener lOListener = new logOutListener();
 
 		JButton requestsButton = new JButton();
 		requestsButton.setLocation(450, 25);
-		requestsButton.setSize(100, 25);
-		
+		requestsButton.setSize(200, 25);
+
 		if (user.getUserType().equals(UserType.ADMIN)) {
 			requestsButton.setText("View Requests");
 		}
-		
+
 		else {
 			requestsButton.setText("Add Request");
 		}
-		
+
+		JButton logOut = new JButton();
+		logOut.setLocation(450, 70);
+		logOut.setSize(200, 25);
+		;
+		logOut.setText("Log Out");
+		logOut.addActionListener(lOListener);
+
 		requestsButton.addActionListener(sRListener);
 
 		// Change location and clear textbox
@@ -202,48 +227,43 @@ public class GUI extends JFrame {
 		scrollList.setSize(500, 300);
 		scrollList.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-		panel.add(filter);
-		panel.add(viewComments);
-		panel.add(scrollList);
-		panel.add(sortCompany);
-		panel.add(sortName);
-		panel.add(requestsButton);
-		panel.add(tBox1);
-		revalidate();
-		repaint();
+		addComponents(filter, logOut, viewComments, scrollList, sortCompany, sortName, requestsButton, tBox1);
 	}
-	
+
 	void viewRequests() {
 		panel.removeAll();
-		
+
 		ActionListener goBackListener = new backListener();
-		
+
 		JButton back = new JButton();
 		back.setLocation(400, 25);
 		back.setSize(100, 25);
 		back.setText("Back");
 		back.addActionListener(goBackListener);
-		
+
 		ArrayList<String> requests = new ArrayList<String>();
 		File requestsFile = new File("requests.txt");
-		Scanner reader;
+		String reqInfo = "";
 		try {
-			reader = new Scanner(requestsFile);
+			Scanner reader = new Scanner(requestsFile);
 			while (reader.hasNext()) {
-				requests.add(reader.nextLine());
+				reqInfo += reader.nextLine();
+				reqInfo += "\t" + reader.nextLine();
+				reqInfo += "\t" + reader.nextLine();
+				requests.add(reqInfo);
 			}
 			reader.close();
 		} catch (FileNotFoundException e) {
 			JOptionPane.showMessageDialog(null, "No Requests");
 			displayApps();
 		}
-		
-		DefaultListModel<String> requestsModel = new DefaultListModel<String>();
-		
+
+		requestsModel = new DefaultListModel<String>();
+
 		for (String request : requests) {
 			requestsModel.addElement(request);
 		}
-		
+
 		requestList = new JList<String>(requestsModel);
 		requestList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
@@ -251,73 +271,62 @@ public class GUI extends JFrame {
 		scrollList.setLocation(0, 100);
 		scrollList.setSize(500, 300);
 		scrollList.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
-		ActionListener acceptListener = new acceptRequestListener();
-		
+
+		ActionListener acceptListener = new requestListener(true);
+		ActionListener denyListener = new requestListener(false);
+
 		JButton acceptRequest = new JButton();
 		acceptRequest.setSize(200, 25);
 		acceptRequest.setLocation(210, 400);
 		acceptRequest.setText("Accept");
 		acceptRequest.addActionListener(acceptListener);
-		
+
 		JButton denyRequest = new JButton();
 		denyRequest.setSize(200, 25);
 		denyRequest.setLocation(420, 400);
-		
-		panel.add(scrollList);
-		panel.add(acceptRequest);
-		panel.add(denyRequest);
-		panel.add(back);
-		
-		revalidate();
-		repaint();
+		denyRequest.setText("Deny");
+		denyRequest.addActionListener(denyListener);
+
+		addComponents(scrollList, acceptRequest, denyRequest, back);
 	}
-	
+
 	void submitRequest() {
 		panel.removeAll();
-		
+
 		JLabel nameLabel = new JLabel(), companyLabel = new JLabel();
 		nameLabel.setLocation(100, 50);
 		nameLabel.setSize(100, 25);
 		nameLabel.setText("App Name:");
-		
+
 		companyLabel.setLocation(100, 150);
 		companyLabel.setSize(100, 25);
 		companyLabel.setText("Company:");
-		
+
 		tBox1.setLocation(250, 50);
 		tBox1.setSize(100, 25);
 		tBox1.setText("");
-		
+
 		tBox2.setLocation(250, 150);
 		tBox2.setSize(100, 25);
 		tBox2.setText("");
-		
+
 		JButton submit = new JButton();
 		submit.setLocation(350, 350);
 		submit.setSize(100, 25);
 		submit.setText("Submit");
-		
+
 		ActionListener listener = new submitRequestListener();
 		ActionListener goBackListener = new backListener();
-		
+
 		JButton back = new JButton();
 		back.setLocation(400, 50);
 		back.setSize(100, 25);
 		back.setText("Back");
-		
+
 		submit.addActionListener(listener);
 		back.addActionListener(goBackListener);
-		
-		panel.add(nameLabel);
-		panel.add(companyLabel);
-		panel.add(tBox1);
-		panel.add(tBox2);
-		panel.add(submit);
-		panel.add(back);
-		
-		revalidate();
-		repaint();
+
+		addComponents(nameLabel, companyLabel, tBox1, tBox2, submit, back);
 	}
 
 	/**
@@ -366,17 +375,12 @@ public class GUI extends JFrame {
 		removeCommentButton.setLocation(420, 400);
 		removeCommentButton.setText("Remove Comment");
 		removeCommentButton.addActionListener(removeComment);
-
+		if (user.getUserType().equals(UserType.BASIC)) {
+			removeCommentButton.setEnabled(false);
+		}
 		tBox1.setLocation(10, 400);
 
-		panel.add(backButton);
-		panel.add(removeCommentButton);
-		panel.add(scrollList);
-		panel.add(appLabel);
-		panel.add(tBox1);
-		panel.add(addCommentButton);
-		revalidate();
-		repaint();
+		addComponents(backButton, removeCommentButton, scrollList, appLabel, tBox1, addCommentButton);
 	}
 
 	private class loadListener implements ActionListener {
@@ -400,25 +404,35 @@ public class GUI extends JFrame {
 	}
 
 	private class logInListener implements ActionListener {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			File file = new File("users.txt");
+			String typeHash = "";
 			try {
 				MessageDigest digest = MessageDigest.getInstance("SHA-256");
 				byte[] userHash = digest.digest(tBox1.getText().getBytes(StandardCharsets.UTF_8));
 				byte[] passHash = digest.digest(tBox2.getText().getBytes(StandardCharsets.UTF_8));
+				String adminHash = DatatypeConverter
+						.printHexBinary(digest.digest("admin".getBytes(StandardCharsets.UTF_8)));
+				String modHash = DatatypeConverter
+						.printHexBinary(digest.digest("mod".getBytes(StandardCharsets.UTF_8)));
 				Scanner sc = new Scanner(file);
 				boolean logInFound = false;
 				while (sc.hasNext()) {
 					String a = sc.next(), b = sc.next();
+					typeHash = sc.next();
 					String a1 = DatatypeConverter.printHexBinary(userHash);
 					String b1 = DatatypeConverter.printHexBinary(passHash);
-					if (a.equals(a1) && b.equals(b1))
+					if (a.equals(a1) && b.equals(b1)) {
 						logInFound = true;
+						break;
+					}
 				}
 				if (logInFound) {
-					user = new User(UserType.ADMIN, tBox1.getText());
+					UserType type = typeHash.equals(adminHash) ? UserType.ADMIN
+							: (typeHash.equals(modHash) ? UserType.MOD : UserType.BASIC);
+					user = new User(type, tBox1.getText());
 					displayApps();
 				} else
 					JOptionPane.showMessageDialog(null, "Invalid Log in");
@@ -449,12 +463,15 @@ public class GUI extends JFrame {
 			// TODO Auto-generated method stub
 			if (!tBox1.getText().equals("") && !tBox2.getText().equals("")) {
 				try {
+					String accountType = adminRadio.isSelected() ? "admin" : (modRadio.isSelected() ? "mod" : "guest");
 					PrintWriter writer = new PrintWriter(new FileOutputStream("users.txt", true));
 					MessageDigest digest = MessageDigest.getInstance("SHA-256");
 					byte[] userHash = digest.digest(tBox1.getText().getBytes(StandardCharsets.UTF_8));
 					byte[] passHash = digest.digest(tBox2.getText().getBytes(StandardCharsets.UTF_8));
+					byte[] accountHash = digest.digest(accountType.getBytes(StandardCharsets.UTF_8));
 					writer.println(DatatypeConverter.printHexBinary(userHash));
 					writer.println(DatatypeConverter.printHexBinary(passHash));
+					writer.println(DatatypeConverter.printHexBinary(accountHash));
 					writer.close();
 					logIn();
 				} catch (FileNotFoundException e) {
@@ -550,50 +567,64 @@ public class GUI extends JFrame {
 		}
 
 	}
-	
+
 	private class submitRequestViewListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			if (user.getUserType().equals(UserType.ADMIN)) {
 				viewRequests();
-			}
-			else {
+			} else {
 				submitRequest();
 			}
 		}
-		
+
 	}
-	
+
 	private class submitRequestListener implements ActionListener {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			try {
 				PrintWriter writer = new PrintWriter(new FileOutputStream("requests.txt", true));
-				writer.println(tBox1.getText() + " " + tBox2.getText() + " " + user.getName());
+				writer.println(tBox1.getText() + "\n" + tBox2.getText() + "\n" + user.getName());
 				writer.close();
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}
-		
-	}
-	
-	private class acceptRequestListener implements ActionListener {
 
-		
+	}
+
+	private class logOutListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			logIn();
+		}
+
+	}
+
+	private class requestListener implements ActionListener {
+
+		boolean accept;
+
+		public requestListener(boolean accept) {
+			this.accept = accept;
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			String app = requestList.getSelectedValue();
-			System.out.println(app);
 			ArrayList<String> newList = new ArrayList<String>();
 			File file = new File("requests.txt");
 			try {
 				Scanner reader = new Scanner(file);
 				while (reader.hasNextLine()) {
 					String entry = reader.nextLine();
+					entry += "\t" + reader.nextLine();
+					entry += "\t" + reader.nextLine();
 					if (!entry.equals(app)) {
 						newList.add(entry);
 					}
@@ -604,30 +635,17 @@ public class GUI extends JFrame {
 					writer.println(entry);
 				}
 				writer.close();
-				db.add((app.substring(0, app.indexOf(' '))), app.substring(app.indexOf(' '), app.lastIndexOf(' ')));
+				refreshList(requestsModel, newList);
+				if (accept) {
+					db.add((app.substring(0, app.indexOf('\t'))),
+							app.substring(app.indexOf('\t'), app.lastIndexOf('\t')));
+				}
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-		}
-	}
-	
-	private class denyRequestListener implements ActionListener {
 
-		ArrayList<String> requests = new ArrayList<String>();
-		String app;
-		
-		public denyRequestListener(ArrayList<String> requests, String app) {
-			this.requests = requests;
-			this.app = app;
 		}
-		
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			
-		}
-		
 	}
 
 }
